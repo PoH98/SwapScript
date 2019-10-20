f6276fbd5dcb551074106573c89807c5f859beec
PoH98 <shunyuan98@gmail.com>
2019-10-21 07:56:44 +0800

    fixed typing error

--- SwitchScript/Program.cs
+++ SwitchScript/Program.cs
@@ -29,7 +29,7 @@ namespace SwitchScript
                 {
                     string[] original, temp;
                     original = File.ReadAllLines(file);
-                    temp = File.ReadAllLines(file.Replace(",csv", ".txt"));
+                    temp = File.ReadAllLines(file.Replace(".csv", ".txt"));
                     File.WriteAllLines(file, temp);
                     File.WriteAllLines(file.Replace(".csv", ".txt"), original);
                     Console.WriteLine("Swaping " + file.Remove(0,file.LastIndexOf('\\')).Replace(".csv", ""));
